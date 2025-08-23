bdd16bc629e70b1087d2829ffb257c75d8372fb0
Dawum Nam <dawumnam@gmail.com>
2025-08-23 19:54:23 +0900

    init

---
+++ eslint.config.ts
@@ -0,0 +1,54 @@
+import js from "@eslint/js";
+import vitest from "@vitest/eslint-plugin";
+import eslintConfigPrettier from "eslint-config-prettier";
+import perfectionist from "eslint-plugin-perfectionist";
+import { configs } from "eslint-plugin-pnpm";
+import pluginPromise from "eslint-plugin-promise";
+import sonarjs from "eslint-plugin-sonarjs";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  ...configs.recommended,
+  perfectionist.configs["recommended-natural"],
+  sonarjs.configs.recommended,
+  pluginPromise.configs["flat/recommended"],
+  {
+    files: ["src/**/*.{js,mjs,cjs}"],
+    extends: [js.configs.recommended],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  {
+    files: ["src/**/*.{ts,mts,cts}"],
+    extends: [
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      eslintPluginUnicorn.configs.recommended,
+    ],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: [
+      "src/**/*.{spec,test}.{js,ts,mjs,mts,cjs,cts}",
+      "src/**/__tests__/**/*.{js,ts,mjs,mts,cjs,cts}",
+    ],
+    ...vitest.configs.recommended,
+  },
+  {
+    ignores: [
+      "dist/**",
+      "node_modules/**",
+      "eslint.config.ts",
+      "vitest.config.ts",
+    ],
+  },
+  eslintConfigPrettier,
+);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+console.log("Hello, TypeScript!");
+
+console.log("Hello, world!");
+
+const a = [1, 2, 3, 4, 5];
+
+console.log(a);
